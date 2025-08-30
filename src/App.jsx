d660e42e2e790f86aca455d6a911a60eed52673c
Rahul Gupta <rg15032003@gmail.com>
2025-08-30 23:48:44 +0530

    initial commit with header and navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import {Routes,Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/blog" element={<Blog/>}/>
+    </Routes>
+    </div>
+  )
+}
+
+export default App
